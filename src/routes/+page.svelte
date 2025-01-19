6a5fb612d6ca7c0a6209bf2bf7b2f92342791c25
Readysoon <p.gallaschik@gmail.com>
2025-01-19 15:41:28 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+  
+    let message = '';
+  
+    onMount(async () => {
+      try {
+        const response = await axios.get('https://scanlytics2-be.fly.dev/');
+        message = response.data.Hello;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        message = 'Error fetching data';
+      }
+    });
+  </script>
+  
+  <h1>Welcome to SvelteKit</h1>
+  <p>{message ? message : 'Loading...'}</p>
