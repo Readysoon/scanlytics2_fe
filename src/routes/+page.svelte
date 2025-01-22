0e999c8b0c03192fec619d63001be3f7a2966b45
Readysoon <p.gallaschik@gmail.com>
2025-01-22 14:45:33 +0100

    removed old testing route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,12 @@
 <script>
     import { onMount } from 'svelte';
     import axios from 'axios';
-  
-    let message = '';
-    let selectedFile = null;
+
     let mlMessage = '';
     let mlSelectedFile = null;
   
-    const handleFileChange = (event) => {
-      selectedFile = event.target.files[0];
-    };
-  
     const handleMLFileChange = (event) => {
       mlSelectedFile = event.target.files[0];
-    };
-  
-    const uploadImage = async () => {
-      if (!selectedFile) {
-        message = 'Please select a file first.';
-        return;
-      }
-  
-      const formData = new FormData();
-      formData.append('file', selectedFile);
-  
-      try {
-        const response = await axios.post('https://scanlytics2-be.fly.dev/surrealdb', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
-  
-        console.log('Response data:', response.data);
-  
-        if (response.data && response.data.length > 0) {
-          message = response.data[0].text || 'No text found';
-        } else {
-          message = 'No data found';
-        }
-  
-      } catch (error) {
-        console.error('Error uploading file:', error);
-        message = 'Error uploading file';
-      }
     };
   
     const uploadToML = async () => {
@@ -74,15 +38,10 @@
         mlMessage = 'Error uploading file to ML';
       }
     };
+    
   </script>
   
-  <h1>Welcome to SvelteKit!!</h1>
-  
-  <!-- Original Upload -->
-  <h2>Upload to SurrealDB</h2>
-  <input type="file" accept="image/*" on:change={handleFileChange} />
-  <button on:click={uploadImage}>Upload Image</button>
-  <p>{message ? message : 'Waiting for upload...'}</p>
+  <h1>Welcome to Scanlytics!</h1>
   
   <!-- New Upload for ML Route -->
   <h2>Upload to ML</h2>
