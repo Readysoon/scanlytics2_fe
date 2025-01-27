2114eedd0cbc4deb7edcbec9bd82e295b84abc10
Readysoon <p.gallaschik@gmail.com>
2025-01-27 16:36:17 +0100

    added (non interactive) guide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,6 +59,37 @@
     </div>
   </div>
 
+  <div class="guide-steps">
+    <div class="step active" id="step-1">
+      <span class="circle">1</span>
+      <div class="text">
+        <span class="headline">Select Image</span>
+        <p>Select an image to upload. JPG, JPEG, GIF, PNG (DICOM soon) are possible</p>
+      </div>
+    </div>
+    <div class="step" id="step-2">
+      <span class="circle">2</span>
+      <div class="text">
+        <span class="headline">Validation and Upload</span>
+        <p>Upon clicking "Upload" Scanlytics anonymizes it and sends it to one of our servers. Learn more about our GDPR compliance.</p>
+      </div>
+    </div>
+    <div class="step" id="step-3">
+      <span class="circle">3</span>
+      <div class="text">
+        <span class="headline">Selection of Report Text</span>
+        <p>Select appearing report texts from the right window.</p>
+      </div>
+    </div>
+    <div class="step" id="step-4">
+      <span class="circle">4</span>
+      <div class="text">
+        <span class="headline">Complete Report</span>
+        <p>Complete and review your report in the middle window.</p>
+      </div>
+    </div>
+  </div>
+
   <footer>
     <div class="footer-content">
       <div class="footer-left">
@@ -181,6 +212,52 @@
     padding: 10px;
     border: 1px solid #ccc;
   }
+
+  .guide-steps {
+    display: flex;
+    justify-content: center; /* Center the steps */
+    align-items: flex-start;
+    margin-top: 20px;
+    padding: 20px 0;
+  }
+
+  .step {
+    display: flex;
+    align-items: flex-start;
+    padding: 10px;
+    transition: background-color 0.3s ease;
+    width: 200px; /* Fixed width for each step */
+    margin: 0 10px; /* Space between steps */
+  }
+
+  .circle {
+    width: 30px;
+    height: 30px;
+    min-width: 30px; /* Prevent compression */
+    min-height: 30px; /* Prevent compression */
+    border-radius: 50%;
+    background-color: #4CAF50;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .headline {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .step.active .circle {
+    background-color: #2196F3;
+  }
+
 
   footer {
     height: 100px;
