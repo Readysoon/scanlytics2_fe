2d90e5bb5582e75725fe0224cf12566d18bfbb55
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-02-18 12:57:50 +0100

    created the navbar with only redirection functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,95 @@
 
 
 
+<script>
+
+</script>
+
+<head>
+    <title>Scanlytics</title>
+</head>
+  
+<body>
+<nav>
+  <div class="logoArea">
+  <img src="/logo.png" alt="Logo" height=30  width=28/>    
+  <h2>Scanlytics</h2>
+</div>
+   <div class="navbar">
+    <a href="/">Home</a> 
+    <a href="/About/">About</a> 
+    <a href="/Demo/">Technology</a> 
+    <a href="/Services/">Services</a>
+
+     <div class="bookCallBtn"><a href="/Booking /" class="bookCalllable">Book a Call</a></div>
+  </div>
+
+</nav>
+
+</body>
+
+
 <style>
+body{
+  background-color: rgb(186, 200, 213);
+}
+
+nav{
+
+    display: flex;
+    height: 35px;
+    align-items: center; 
+    justify-content: space-between;
+
+}
+.logoArea{
+
+  height: 100%;
+  width: 8%;
+  display: flex;
+  align-items: center;
+  gap: 8%;
+  padding-left: 1%;
+  font-family: system-ui;
+
+}
+
+.navbar{
+
+    width: 27%;
+    height: 100%;
+    display: flex; 
+    justify-content: space-between;
+    align-items: center; 
+    padding-right: 1%;
+    
+
+}
+
+a{
+  text-decoration: none;
+  color: black; 
+  font-weight: 500;
+  font-family: system-ui;
+}
+
+.bookCalllable{
+  color: white;
+  font-size: 14px;
+  font-weight: 400;
+   font-family: system-ui;
+}
+.bookCallBtn{
+  background-color: rgb(0, 0, 0);
+  width: 20%;
+  height: 80%;
+  display: flex; 
+  justify-content: center;
+  align-items: center; 
+  border: 1px solid black;
+  border-radius: 40px;
+  color: white;
+}
+
 
 </style>
