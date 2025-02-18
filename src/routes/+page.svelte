a81d8a7a33e48b3c40c122ab05a0220cc0f756ac
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-02-18 14:07:08 +0100

    done with the first design instance for scanlytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,95 +1,230 @@
-
-
-
 <script>
-
 </script>
 
 <head>
-    <title>Scanlytics</title>
+	<title>Scanlytics</title>
 </head>
-  
+
 <body>
-<nav>
-  <div class="logoArea">
-  <img src="/logo.png" alt="Logo" height=30  width=28/>    
-  <h2>Scanlytics</h2>
-</div>
-   <div class="navbar">
-    <a href="/">Home</a> 
-    <a href="/About/">About</a> 
-    <a href="/Demo/">Technology</a> 
-    <a href="/Services/">Services</a>
+	<nav>
+		<div class="logoArea">
+			<img src="/logo.png" alt="Logo" height="30" width="28" />
+			<h2>Scanlytics</h2>
+		</div>
+		<div class="navbar">
+			<a href="/" class="homeBtn">Home</a>
+			<a href="/About/">About</a>
+			<a href="/Demo/">Technology</a>
+			<a href="/Services/">Services</a>
 
-     <div class="bookCallBtn"><a href="/Booking /" class="bookCalllable">Book a Call</a></div>
-  </div>
+			<div class="bookCallBtn"><a href="/Booking /" class="bookCalllable">Book a Call</a></div>
+		</div>
+	</nav>
 
-</nav>
+	<div class="mainSection">
+		<div class="leftContentSection">
+			<div class="leftContentHeader">
+				<div class="leftContentHeaderArea">
+					<p>Worlds most Adopted AI</p>
+				</div>
+			</div>
 
+			<div class="middleContentSection">
+				<div class="middleContentTitleArea">
+					<div class="mainTitle">Revolutionizing Healthcare with AI</div>
+				</div>
+
+				<div class="middleContentSubTextArea">
+					<div class="subtext">
+						Artificial intelligence is transforming radiology, enhancing accuracy, efficiency, and
+						patient care. AI-powered algorithms can analyze medical images with remarkable
+						precision, detecting abnormalities such as tumors and fractures faster than ever before.
+						This not only speeds up diagnosis but also reduces human error, supporting radiologists
+						in making more informed decisions. By automating routine tasks, AI allows healthcare
+						professionals to focus more on complex cases and patient interactions. As AI continues
+						to evolve, it promises to revolutionize radiology, leading to earlier disease detection,
+						personalized treatment plans, and improved healthcare outcomes worldwide.
+					</div>
+				</div>
+			</div>
+
+      <div class="leftContentBtnArea">
+          
+          <div class="leftBtnSection">
+          
+          
+          	<div class="leftBtn_1"><a href="/Booking /" class="bookCalllable">Book a Call</a></div>
+
+            <div class="leftBtn_2"><a href="/Booking /" class="leftappointmentbtn">Appointment</a></div>
+
+          </div>
+			</div>
+		</div>
+
+		<div class="rightContentSeciton"></div>
+	</div>
 </body>
 
-
 <style>
-body{
-  background-color: rgb(186, 200, 213);
-}
+	body {
+		background-color: rgb(186, 200, 213);
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	}
 
-nav{
+	nav {
+		display: flex;
+		width: '100%';
+		height: 35px;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.mainSection {
+		height: 690px;
+		margin-top: 30px;
+	}
+	.logoArea {
+		height: 100%;
+		width: 8%;
+		display: flex;
+		align-items: center;
+		gap: 8%;
+		padding-left: 1%;
+		font-family: system-ui;
+	}
 
-    display: flex;
-    height: 35px;
-    align-items: center; 
-    justify-content: space-between;
+	.navbar {
+		width: 27%;
+		height: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding-right: 1%;
+	}
 
-}
-.logoArea{
+	a {
+		text-decoration: none;
+		color: rgb(77, 77, 77);
+		font-weight: 500;
+		font-family: system-ui;
+	}
 
-  height: 100%;
-  width: 8%;
-  display: flex;
-  align-items: center;
-  gap: 8%;
-  padding-left: 1%;
-  font-family: system-ui;
+	.bookCalllable {
+		color: white;
+		font-size: 14px;
+		font-weight: 400;
+		font-family: system-ui;
+	}
+	.bookCallBtn {
+		background-color: rgb(0, 0, 0);
+		width: 20%;
+		height: 80%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid black;
+		border-radius: 40px;
+		color: white;
+	}
 
-}
+	.homeBtn {
+		color: black;
+	}
 
-.navbar{
+	.leftContentSection {
+		width: 60%;
+		height: 100%;
+	}
 
-    width: 27%;
-    height: 100%;
+	.leftContentHeader {
+		width: 100%;
+		height: 7%;
+		display: flex;
+		align-items: center;
+	}
+	.leftContentHeaderArea {
+		background-color: rgb(226, 241, 252);
+		width: 20%;
+		height: 60%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid black;
+		border-radius: 40px;
+		color: black;
+		margin-left: 1%;
+	}
+
+	.middleContentSection {
+		width: 100%;
+		height: 80%;
+	}
+
+	.middleContentTitleArea {
+		width: 100%;
+		height: 76%;
+	}
+
+	.middleContentSubTextArea {
+		width: 70%;
+		height: 24%;
+		color: rgb(48, 48, 48);
+    padding: 1%;
+
+	}
+	.mainTitle {
+		width: 70%;
+		height: 100%;
+		padding-left: 1%;
+		font-size: 100px;
+		font-family: system-ui;
+		color: black;
+	}
+
+  .leftContentBtnArea{
+		width: 70%;
+		height: 13%;
+		color: rgb(48, 48, 48);
+    padding: 1%;
+  }
+
+  .leftBtnSection{
+		width: 70%;
+		height: 100%;
+		color: rgb(48, 48, 48);
     display: flex; 
-    justify-content: space-between;
-    align-items: center; 
-    padding-right: 1%;
-    
+    align-items: center;
+    gap: 5%
 
-}
+  }
 
-a{
-  text-decoration: none;
-  color: black; 
-  font-weight: 500;
-  font-family: system-ui;
-}
+  .leftBtn_1{
+    	background-color: rgb(0, 0, 0);
+		width: 30%;
+		height: 38%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid black;
+		border-radius: 40px;
+		color: white;
+  }
 
-.bookCalllable{
-  color: white;
-  font-size: 14px;
-  font-weight: 400;
-   font-family: system-ui;
-}
-.bookCallBtn{
-  background-color: rgb(0, 0, 0);
-  width: 20%;
-  height: 80%;
-  display: flex; 
-  justify-content: center;
-  align-items: center; 
-  border: 1px solid black;
-  border-radius: 40px;
-  color: white;
-}
+  .leftBtn_2{
+		width: 30%;
+		height: 38%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid black;
+		border-radius: 40px;
+		color: black;
+  }
+  .leftappointmentbtn{
+    		color: black;
+        
 
-
+  }
 </style>
