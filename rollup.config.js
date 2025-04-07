09b271b68dc58cd9f052e53ab1f798dab901b5fb
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-04-08 01:52:43 +0200

    updated rollup file for build process

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import json from '@rollup/plugin-json'
 
 // under the plugin line :
 plugins: [
+  replace({
+    'process.env.SECRET_OPENAIKEY': JSON.stringify(process.env.SECRET_OPENAIKEY),
+    preventAssignment: true,  // Required to avoid warnings
+  }),
   json({
     compact: true
   })
