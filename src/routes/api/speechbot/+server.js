a7bfa31986108f7258c32a2b855cb6ac70d252fe
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-04-09 19:36:35 +0200

    updated the google connection

--- src/routes/api/speechbot/+server.js
+++ src/routes/api/speechbot/+server.js
@@ -2,31 +2,27 @@ import { json } from '@sveltejs/kit';
 // import textToSpeech from '@google-cloud/text-to-speech';
 import { TextToSpeechClient } from '@google-cloud/text-to-speech';
 import { writeFile } from 'node:fs/promises';
+import { env } from '$env/dynamic/private';
 import path from 'path';
 
-const client = new TextToSpeechClient();
-
+// const client = new TextToSpeechClient();
+const client = new TextToSpeechClient({
+	keyFilename: path.resolve(env.GOOGLE_CLOUD_JSON_KEY)
+});
 
 const handlespeechBot = async (botText) => {
-	// Creates a client
-	// const client = new textToSpeech.TextToSpeechClient({
-	// 	credentials: {
-	// 		client_email: 'your-service-account@project-id.iam.gserviceaccount.com',
-	// 		private_key: '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n'
-	// 	}
-
-
 	// The text to synthesize
-	const text =  botText;
+	const text = botText;
 
 	// Construct the request
 	const request = {
 		input: { text },
 		// Select the language and SSML voice gender (optional)
-		voice: { 
+		voice: {
 			languageCode: 'de-DE',
-			name: 'de-DE-Wavenet-B', 
-			ssmlGender: 'NEUTRAL' },
+			name: 'de-DE-Wavenet-B',
+			ssmlGender: 'NEUTRAL'
+		},
 		// Select the type of audio encoding
 		audioConfig: { audioEncoding: 'MP3' }
 	};
@@ -34,20 +30,19 @@ const handlespeechBot = async (botText) => {
 	// Performs the text-to-speech request
 	const [response] = await client.synthesizeSpeech(request);
 
-	const filePath = path.join('static', 'output.mp3' )
-    const audioFile = await writeFile(filePath, response.audioContent, 'binary');
+	const filePath = path.join('static', 'output.mp3');
+	const audioFile = await writeFile(filePath, response.audioContent, 'binary');
 	console.log('Audio content written to file: output.mp3');
 
-	return '/output.mp3'
+	return '/output.mp3';
 };
-
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	const { data } = await request.json();
-	
-    console.log('data from gpt in areach ttS', data);
-	const botAudioFileData = await handlespeechBot(data)
 
-    return json({success: true, audioUrl: botAudioFileData})
+	console.log('data from gpt in areach ttS', data);
+	const botAudioFileData = await handlespeechBot(data);
+
+	return json({ success: true, audioUrl: botAudioFileData });
 }
